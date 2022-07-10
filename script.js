698604294b40cc33d028d7d571ac643216862365
Dayvin30 <dayvin30@gmail.com>
2022-07-10 02:50:20 +0200

    all files

---
+++ script.js
@@ -0,0 +1,9 @@
+/*function changeImage() {
+
+var image= "<?php echo(randomChampion())?>" //call the php add function
+var url="images/";
+
+
+document.getElementById("changeme").src = url + image;
+
+} */
